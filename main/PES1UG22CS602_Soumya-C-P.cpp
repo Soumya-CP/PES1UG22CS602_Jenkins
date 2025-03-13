b0cef69584b77b773c27aaf6084169ebdb2b94ab
Soumya C P <167528708+Soumya-CP@users.noreply.github.com>
2025-03-13 17:34:29 +0530

    Add files via upload

---
+++ main/PES1UG22CS602_Soumya-C-P.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"\nHello, Myself Soumya C P.\nSRN: PES1UG22CS602\n";
+	for(int i=1;i<6;i++){
+		cout<<i<<"\n";
+	}
+	
+	return 0;
+}
