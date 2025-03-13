7d67dc07e1800384fa3b393f4fece4956812deca
Soumya C P <167528708+Soumya-CP@users.noreply.github.com>
2025-03-13 16:35:26 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "SOUMYA C P\n PES1UG2CCS602\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
